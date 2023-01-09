8f868945bc82f87cab42e246ef4cf2e7045a8109
KATI LEE <katherineleefoley@gmail.com>
2023-01-09 00:25:39 +0000

    doing stuff in the app.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+
+  render(){
+    return(
+      //add components
+      <>
+      <p>
+        
+      </p>
+      </>
+    )
+
+  }
+
+}
+
+export default App;
