e3b540c37e2651757121c985e51b78b2228c0785
KATI LEE <katherineleefoley@gmail.com>
2023-01-12 01:36:31 +0000

    creating an array for to push into, trying to figure out where my structuring error lays

--- src/Main.js
+++ src/Main.js
@@ -4,9 +4,30 @@ import data from './data.json';
 
 class Main extends React.Component {
     render (){
-        return(
-            <>
-            <HornedBeast
+        let beast = [];
+        data.forEach((newBeast, index)) => {
+            beast.push(
+                <Beast beastName={newBeast.name} image_url={newBeast.image_url} key={index}/>
+            );
+        }
+            return(
+            <main>
+            {beast}
+                </main>
+                );
+            }
+        }
+
+
+export default Main;
+
+
+
+
+
+
+
+{/* <HornedBeast
                 title={"UniWhal"}
                 image_url={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"}
                 description={"A unicorn and a narwhal nuzzling their horns"}
@@ -20,11 +41,4 @@ class Main extends React.Component {
                 title={"Unicorn Head"}
                 image_url={"https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"}
                 description={"Someone wearing a very silly unicorn head mask"}
-                />
-                </>
-                );
-            }
-        }
-
-
-export default Main;
+                /> */}
